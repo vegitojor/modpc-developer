8a7b6ada3f17195d7c1f4896d0aeb9c62c4f2304
vegitojor <j.velazquez1985@gmail.com>
2018-04-10 20:40:19 -0300

    se arrela falla en scrip de provincia y localidades

---
+++ base de datos/2018-04-10-tabla-provincia.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS `provincia`;
+CREATE TABLE IF NOT EXISTS `provincia` (
+  `id_provincia` int(10) NOT NULL AUTO_INCREMENT,
+  `provincia` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_provincia`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+
+INSERT INTO `provincia` (`id_provincia`, `provincia`) VALUES
+(1, 'Buenos Aires'),
+(2, 'Buenos Aires-GBA'),
+(3, 'Capital Federal'),
+(4, 'Catamarca'),
+(5, 'Chaco'),
+(6, 'Chubut'),
+(7, 'Córdoba'),
+(8, 'Corrientes'),
+(9, 'Entre Ríos'),
+(10, 'Formosa'),
+(11, 'Jujuy'),
+(12, 'La Pampa'),
+(13, 'La Rioja'),
+(14, 'Mendoza'),
+(15, 'Misiones'),
+(16, 'Neuquén'),
+(17, 'Río Negro'),
+(18, 'Salta'),
+(19, 'San Juan'),
+(20, 'San Luis'),
+(21, 'Santa Cruz'),
+(22, 'Santa Fe'),
+(23, 'Santiago del Estero'),
+(24, 'Tierra del Fuego'),
+(25, 'Tucumán');
+
