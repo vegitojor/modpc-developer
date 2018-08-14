5292848bdd38bb0a88c5ddf2fdddc3d33f318bae
Vegitojor <j.velazquez1985@gmail.com>
2018-08-13 21:16:06 -0300

    .gitignore is now working

---
+++ clases/ConexionBDClass.php
@@ -0,0 +1,36 @@
+<?php 
+
+Class ConexionBD {
+
+	private $servidor = "localhost";
+	private $usuario = "root";
+	private $pass = "abrh++++";
+	private $bd = "modpc";
+
+	private $conexion;
+
+	function __construct(){
+		$this->conexion = mysqli_connect($this->servidor, $this->usuario, $this->pass, $this->bd);
+	}
+
+	function getConexion(){
+		return $this->conexion;
+	}
+
+	function ejecutarConsulta($sql){
+		$query = mysqli_query($this->conexion, $sql);
+		return $query;
+	}
+
+	function prepare($sql){
+		$resultado = mysqli_prepare($this->conexion, $sql);
+		return $resultado;
+	}
+
+	function cerrarConexion(){
+        mysqli_close($this->conexion);
+    }
+
+}
+
+?>
