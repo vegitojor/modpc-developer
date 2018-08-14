4ccefa39b623985d18f876506c1e3f07a9eadfa8
Vegitojor <j.velazquez1985@gmail.com>
2018-08-13 21:41:44 -0300

    corregido mail contacto

--- librerias/formulario_contacto/contact_me.php
+++ librerias/formulario_contacto/contact_me.php
@@ -17,7 +17,7 @@ $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'boyerosdebernanonthue@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'info@modpc.com.ar'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "ModPc desde el formulario de contacto:  $name";
 $email_body = "Tu has recibido un mensaje desde el formulario de contacto del sitio ModPc.\n\n"."Acá estan los detalles:\n\nNombre: $name\n\nEmail: $email_address\n\nMensaje:\n$message";
 $headers = "From: info@MODPC.com.ar\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
